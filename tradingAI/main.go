97e32fab062bb2f668c078a7b5640aefdb61edc4
Tegar Cahya Bayu Siregar <64503005+MrCoeg@users.noreply.github.com>
2022-02-03 16:53:05 +0700

    Update main.go

--- tradingAI/main.go
+++ tradingAI/main.go
@@ -41,69 +41,70 @@ type Ticker struct {
 	Server_time int    `json:"server_time"`
 }
 
-type test struct {
-}
-
-type Test interface {
-}
-
 func main() {
-	chart := Chart{
-		Pattern: make([]func(candle []Candle) string, 6),
+
+	// Init
+	CoinNames := []string{
+		"btc_idr",
+		"Abyss_idr",
 	}
+	charts := make([]Chart, 2)
+	for i := 0; i < len(charts); i++ {
+		charts[i] = Chart{
+			Pattern: make([]func(candle []Candle) string, 6),
+		}
+		charts[i].Pattern = []func(candle []Candle) string{
+			func(candle []Candle) string {
+				length := len(candle)
+				var newCandle []Candle
+				if length < 3 {
+					return "candle is not enough to analyse"
+				} else if length > 3 {
 
-	var chartPattern = []func(candle []Candle) string{
-		func(candle []Candle) string {
-			length := len(candle)
-			var newCandle []Candle
-			if length < 3 {
-				return "candle is not enough to analyse"
-			} else if length > 3 {
+					for i := length - 3; i < length-1; i++ {
+						newCandle = append(newCandle, candle[i])
+					}
 
-				for i := length - 3; i < length-1; i++ {
-					newCandle = append(newCandle, candle[i])
+					high := [2]int{newCandle[0].last, newCandle[1].last}
+					// low := [2]int{newCandle[0].low, newCandle[1].low}
+
+					if high[0] > high[1] {
+						return "CLOUD COVER - STOP BUY - SELL NOW"
+					}
+				} else {
+					newCandle = candle
+					high := [2]int{newCandle[0].last, newCandle[1].last}
+					// low := [2]int{newCandle[0].low, newCandle[1].low}
+
+					if high[0] > high[1] {
+						return "CLOUD COVER - STOP BUY - SELL NOW"
+					}
 				}
 
-				high := [2]int{newCandle[0].last, newCandle[1].last}
-				// low := [2]int{newCandle[0].low, newCandle[1].low}
-
-				if high[0] > high[1] {
-					return "CLOUD COVER - STOP BUY - SELL NOW"
-				}
-			} else {
-				newCandle = candle
-				high := [2]int{newCandle[0].last, newCandle[1].last}
-				// low := [2]int{newCandle[0].low, newCandle[1].low}
-
-				if high[0] > high[1] {
-					return "CLOUD COVER - STOP BUY - SELL NOW"
-				}
-			}
-
-			return "seluw"
-		},
-		func(candle []Candle) string {
-			return ""
-		},
-		func(candle []Candle) string {
-			return ""
-		},
-		func(candle []Candle) string {
-			return ""
-		},
-		func(candle []Candle) string {
-			return ""
-		},
-		func(candle []Candle) string {
-			return ""
-		},
+				return "seluw"
+			},
+			func(candle []Candle) string {
+				return ""
+			},
+			func(candle []Candle) string {
+				return ""
+			},
+			func(candle []Candle) string {
+				return ""
+			},
+			func(candle []Candle) string {
+				return ""
+			},
+			func(candle []Candle) string {
+				return ""
+			},
+		}
+		charts[i].candles = append(charts[i].candles, Candle{})
 	}
-
-	chart.Pattern = chartPattern
-
-	chart.candles = append(chart.candles, Candle{})
 	interval := 10
 	frameRate := 5
+
+	// Request Data
 	for {
 		res, err := http.Get("https://indodax.com/api/tickers")
 
@@ -126,21 +127,31 @@ func main() {
 			panic(err)
 		}
 
-		// for i := 0; i < len(cryptocoins.CoinNames); i++ {
-		// 	coinName := cryptocoins.CoinNames[i]
-
-		// }
-
-		chart.candles[len(chart.candles)-1].Update(cryptocoins.Coins.Btc_idr)
-		fmt.Println(len(chart.candles)-1, chart.candles[len(chart.candles)-1].last)
-		chart.CreateNewCandle(frameRate, interval, chartPattern)
+		// Update and Analyse
+		for i := 0; i < len(charts); i++ {
+			UpdateChart(&charts[i], &cryptocoins, frameRate, interval, CoinNames[i])
+		}
+		fmt.Println()
 		time.Sleep(time.Duration(frameRate) * time.Second)
 	}
 
 }
 
+func UpdateChart(charts *Chart, cryptocoins *Cryptocoins, frameRate int, interval int, coinName string) {
+	switch coinName {
+	case "btc_idr":
+		charts.candles[len(charts.candles)-1].Update(cryptocoins.Coins.Btc_idr)
+		fmt.Println(len(charts.candles)-1, charts.candles[len(charts.candles)-1].last)
+		charts.CreateNewCandle(frameRate, interval, charts.Pattern)
+	case "Abyss_idr":
+		charts.candles[len(charts.candles)-1].Update(cryptocoins.Coins.Abyss_idr)
+		fmt.Println(len(charts.candles)-1, charts.candles[len(charts.candles)-1].last)
+		charts.CreateNewCandle(frameRate, interval, charts.Pattern)
+	}
+}
+
 func (ch *Chart) CreateNewCandle(frameRate, interval int, callback []func(candle []Candle) string) {
-	// var cp = ChartPattern{}
+
 	ch.timeSeconds += frameRate
 	if ch.timeSeconds >= interval {
 		ch.candles[len(ch.candles)-1].close = ch.candles[len(ch.candles)-1].last
@@ -148,7 +159,10 @@ func (ch *Chart) CreateNewCandle(frameRate, interval int, callback []func(candle
 
 		for i := 0; i < len(ch.Pattern); i++ {
 			message := callback[i](ch.candles)
-			fmt.Println(message)
+			if message != "" {
+				fmt.Println(message)
+			}
+
 		}
 
 		ch.timeSeconds = 0
